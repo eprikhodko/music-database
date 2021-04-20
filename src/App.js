373546022ea450ac230b104179a44c2585a9d1f5
eprikhodko <eprikhodkodev@gmail.com>
2021-04-20 16:58:18 +0300

    Add basic home page structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
 
-function App() {
+import './App.css'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AlbumPreview />
+      <RecentlyAddedAlbums />
     </div>
   );
 }
+
+const Header = () => {
+
+  const [query, setQuery] = useState("")
+
+  const handleChange = event => {
+    return setQuery(event.target.value)
+  }
+
+  const searchMusic = (event) => {
+    event.preventDefault()
+    console.log("submitted") 
+  }
+
+  return (
+    <div className="container-search">
+      <form className="form" onSubmit={searchMusic}>
+          <label className="label" htmlFor="query">music search</label>
+          <input 
+              className="search-input" 
+              type="text" 
+              name="query"
+              placeholder="i.e. Ramones" 
+              value={query} 
+              onChange={handleChange}
+          />
+          <button className="button-search" type="submit">Search</button>
+      </form>
+    </div>
+  )
+}
+
+const AlbumPreview = () => {
+    return (
+      <div>
+        <img src="http://placekitten.com/250/250" alt="album cover"/>
+      </div>
+    )
+}
+
+const RecentlyAddedAlbums = () => {
+  return (
+    <div>
+      <h2>Recently added albums</h2>
+        <img src="http://placekitten.com/245/245" alt="album cover"/>
+        <img src="http://placekitten.com/246/246" alt="album cover"/>
+      </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
